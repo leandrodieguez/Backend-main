ed27e09934d5c27d44ac9c8fd8651aa88f794fd7
leandrodieguez <leandro1dieguez@gmail.com>
2022-09-04 17:24:23 -0300

    Cors corregidos

--- src/main/java/com/portfolio/ld/Controller/CProyecto.java
+++ src/main/java/com/portfolio/ld/Controller/CProyecto.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendld-fef11.web.app")
 public class CProyecto {
     @Autowired
     Sproyecto sProyecto;
--- src/main/java/com/portfolio/ld/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/ld/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontendld-fef11.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
